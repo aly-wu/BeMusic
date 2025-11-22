251da2f61466364955655331b6dc3b6074da74ff
aly-wu <zjwualyssa@gmail.com>
2025-11-21 20:44:01 -0800

    + preliminary interfaces

---
+++ src/BeMusic/UserDatabase.java
@@ -0,0 +1,17 @@
+package BeMusic;
+
+// An interface for a data structure that stores all BeMusic users
+// Any class implementing UserDatabase should be implemented as a graph. 
+public interface UserDatabase {
+
+    /**
+     * 
+     */
+    public void addUser();
+
+    /**
+     * 
+     */
+    public void deleteUser();
+    
+}
