c4db733325fbf2059450f7adb26ca2a9efe88296
aly-wu <zjwualyssa@gmail.com>
2025-11-29 16:47:52 -0800

    + building Song + ListeningHistory classes

--- src/BeMusic/ListeningHistory.java
+++ src/BeMusic/ListeningHistory.java
@@ -1,11 +1,59 @@
 package BeMusic;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Queue;
+import java.util.PriorityQueue;
+
 // An interface for a data structure that stores a given BeMusic user's listening history
+
 /**
- * Any objecct that implements listening history should have two underying search trees/dictionaries
- *      1) one for song history with date keys and song values
- *      2) one for artist history with artist keys and number of times listened to said artist values
+ * Two underlying data structures 
+ *      1) songHistory: a dictionary with month/year keys and priority queue values of songs sorted by date (most recent song listened to at top of PQ)
+ *      2) artistHistory: a dictionary with artist keys and number of times listened to said artist values (every time we add a song, we must search for that artist in their song history --> map to store)
+ * 
+ * TODO: maybe we keep track of top artist and maxlistens, every day we add a song, we check if that artist's value exceed max listens,
+ *      if it does, we change the top artist. this could be expanded to top 3, but would be quite annoying. 
+ *      this would work for all time top artist, but what about top artist for the month? 
+ *      or, when getting top artist for the month, get the songHistory for the month, iterate through max 30 entries and keep track of artists.
+ *      return topArtist
 */
-public interface ListeningHistory {
-    public void addSong(String title, String artist, Date date);
+public class ListeningHistory {
+    // Instance variables
+    HashMap<Date, Queue<Song>> songHistory;
+    HashMap<String, Integer> artistHistory;
+
+    // Constructor
+    public ListeningHistory(){
+        this.songHistory = new HashMap<Date, Queue<Song>>();
+        this.artistHistory = new HashMap<String, Integer>();
+    }
+
+    /**
+     * @pre all songs are distinct objects (since same songs listened on different days are still "different songs")
+     * @param song
+     */
+    public void addSong(Song song, Date month){
+        // Add song to songHistory 
+        Date MMyyyy = song.date.getMonth() + getYear(); // TODO: figure this out
+        if (!songHistory.containsKey(MMyyyy)){ // beginning of a new month
+            Queue<Song> pq = new PriorityQueue<>();
+            pq.add(song);
+            songHistory.put(MMyyyy, pq);
+        } else { // mid-month
+            Queue<Song> pq = songHistory.get(MMyyyy);
+            pq.add(song);
+            songHistory.put(MMyyyy, pq);
+        }
+
+        // Add song to artistHistory
+        String artist = song.artist;
+        if (!artistHistory.containsKey(artist)){ // new artists
+            artistHistory.put(artist,1);
+        } else { // old artist, increase by 1
+            artistHistory.put(artist, artistHistory.get(artist) + 1);
+        }
+
+    }
+
 }
---
+++ src/BeMusic/Song.java
@@ -0,0 +1,91 @@
+package BeMusic;
+
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Song {
+    // Instance variables
+    public final String title;
+    public final String artist;
+    public final Date date; // the same song listened to on different days IS a different song object
+    // TODO: other things we may want to store like album cover, nicheness score, etc.
+
+    /**
+     * 
+     * @param title
+     * @param artist
+     * @param date
+     */
+    public Song(String title, String artist, String date){
+        this.title = title;
+        this.artist = artist;
+
+        // Date in format from excel as mm/d(d)/yyyy, need to standardize as mm/dd/yyyy
+        String[] dateSplit = date.split("/");
+        String day = dateSplit[1];
+        if (day.length() == 1){
+            dateSplit[1] = "0" + day;
+        }
+        String formattedDate = String.join("-", dateSplit);
+        SimpleDateFormat formatter = new SimpleDateFormat("MM-dd-yyyy", Locale.ENGLISH);
+        try{
+            Date dateObj = formatter.parse(formattedDate); 
+            this.date = dateObj;
+        } catch (ParseException e){
+            System.out.println("error parsing date" + e);
+            this.date = null; // TODO: figure this out...
+        }
+    }
+
+    /**
+     * NOTE: In Java's PriorityQueue with natural ordering/comparator, the head is the smallest (minimal) element
+     * @Override
+     */
+    public int compareTo(Song that){
+        // If after, then more recent --> "smaller"
+        if (this.date.after(that.date)){
+            return -1;
+        
+        // If before, then older --> "bigger"
+        } if (this.date.before(that.date)){
+            return 1;
+
+        // Otherwise, same date
+        } return 0;
+    }
+
+    /**
+    * @Override
+    */
+    public String toString(){
+        return "\"" + this.title + "\" by " + this.artist; 
+    }
+
+    /**
+     * Tests the Song class. 
+     * TODO: TEST!
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("-------testing compareTo()-------");
+        Queue<Song> priorityQueue = new PriorityQueue<>();
+     
+        // insert elements in the queue
+        priorityQueue.add(new Song("EoO", "Bad Bunny", "11/29/2025"));
+        priorityQueue.add(new Song("Heroine", "Azamiah", "10/15/2025"));
+        priorityQueue.add(new Song("Fall In Love (Your Funeral)", "Erykah Badu", "11/11/2025"));
+        priorityQueue.add(new Song("Care for You", "The Marias", "10/31/2025"));
+
+        // Should return songs starting from most recently listened
+        while (!priorityQueue.isEmpty()) {
+            System.out.print(priorityQueue.poll() + " ");
+        }
+       
+    }
+}
